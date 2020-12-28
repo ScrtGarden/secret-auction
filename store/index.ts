29d07a3f8f7b6293f30e857b0778feff7cc44482
hieulai <>
2020-12-29 09:22:51 +1100

    remove deny list

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,9 @@
 import { State, Store, createStore, persist } from 'easy-peasy'
-import { useMemo } from "react"
+import { useMemo } from 'react'
 
 import { AuthModel, AuthState } from './auth/auth.models'
-import authState  from "./auth/auth.state"
-import authStore  from "./auth/auth.store"
+import authState from './auth/auth.state'
+import authStore from './auth/auth.store'
 
 export interface StoreModel {
   auth: AuthModel
@@ -16,7 +16,7 @@ export interface StoreState {
 let store: Store | undefined
 
 const initialStoreState: StoreState = {
-  auth: authState
+  auth: authState,
 }
 
 const model = persist(
@@ -25,12 +25,12 @@ const model = persist(
   },
   {
     storage: 'localStorage',
-    deny: ["auth"]
+    // deny: ["auth"]
   }
 )
 
 // add this if you want to inject store
-// const getStore = () => store 
+// const getStore = () => store
 
 const initStore = (initialState = initialStoreState) =>
   createStore(model, {
@@ -39,29 +39,29 @@ const initStore = (initialState = initialStoreState) =>
     // injections: { getStore }, // add this if you want to inject store
   })
 
-  export const initializeStore = (preloadedState: StoreState) => {
-    let _store = store ?? initStore(preloadedState)
-  
-    // After navigating to a page with an initial Redux state, merge that state
-    // with the current state in the store, and create a new store
-    if (preloadedState && store) {
-      _store = initStore({
-        ...store.getState(),
-        ...preloadedState,
-      })
-      // Reset the current store
-      store = undefined
-    }
-  
-    // For SSG and SSR always create a new store
-    if (typeof window === 'undefined') return _store
-    // Create the store once in the client
-    if (!store) store = _store
-  
-    return _store
+export const initializeStore = (preloadedState: StoreState) => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
   }
-  
-  export const useStore = (initialState: StoreState) => {
-    const store = useMemo(() => initializeStore(initialState), [initialState])
-    return store
-  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+export const useStore = (initialState: StoreState) => {
+  const store = useMemo(() => initializeStore(initialState), [initialState])
+  return store
+}
