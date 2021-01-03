e82f4504594d6a4fe38418cca89305de3a9d8582
hieulai <>
2021-01-03 15:14:30 +1100

    add more constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -33,8 +33,22 @@ const AUCTION_CONTRACT_ID = 171
 // const FACTORY_CONTRACT_ID = 170
 // const FACTORY_CONTRACT_ADDRESS = 'secret1s0l9vyh8futvlctv4nup8pa8lfz9u4rgcye7c2'
 
-const FACTORY_CONTRACT_ID = 176
-const FACTORY_CONTRACT_ADDRESS = 'secret1q5dadwv9dtvqde7l8tvkm0zlgn0hqr9vkla5pn'
+const FACTORY_CONTRACT_ID = 179
+const FACTORY_CONTRACT_ADDRESS = 'secret1fr3m662gpe97jw2xgg4hfkn8lufq4hf8k6zn0a'
+
+const OPEN_OWNER_ACTIONS = {
+  consign: 'Consign',
+  finalise: 'Finalise & Close',
+}
+
+const OPEN_BIDDER_ACTIONS = {
+  retract: 'Retract Bid',
+  bid: 'Bid',
+}
+
+const CLOSED_AUCTION_ACTIONS = {
+  return: 'Return All',
+}
 
 export {
   MAP_ROUTE_AND_COLOR,
@@ -42,4 +56,6 @@ export {
   AUCTION_CONTRACT_ID,
   FACTORY_CONTRACT_ID,
   FACTORY_CONTRACT_ADDRESS,
+  OPEN_OWNER_ACTIONS,
+  OPEN_BIDDER_ACTIONS,
 }
