3083dffa66d055a895589d2f2e51f0406e2bebff
hieulai <>
2021-01-16 16:51:21 +1100

    parse error message

---
+++ utils/parseErrorMessage.ts
@@ -0,0 +1,13 @@
+const parseErrorMessage = (message: string) => {
+  let parsedMessage = 'Something went wrong. Please try again later.'
+
+  if (message.includes('Request rejected')) {
+    parsedMessage = 'Looks like you rejected the request.'
+  } else if (message.includes('out of gas')) {
+    parsedMessage = 'Sorry you ran out of gas.'
+  }
+
+  return parsedMessage
+}
+
+export default parseErrorMessage
