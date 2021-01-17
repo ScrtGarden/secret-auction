b6dcc11f28dd29de7169868cac9c5443b6c99ef6
hieulai <>
2021-01-17 11:17:18 +1100

    update ui

--- src/components/Common/InputWithSymbol/styles.ts
+++ src/components/Common/InputWithSymbol/styles.ts
@@ -5,7 +5,6 @@ const TokenLabel = styled.div`
   color: ${(props) => props.theme.palette.white};
   display: flex;
   background-color: ${(props) => props.theme.palette.grey[800]};
-  border: 1px solid ${(props) => props.theme.palette.grey[300]};
   border-bottom-right-radius: ${(props) => props.theme.borderRadii.md};
   border-top-right-radius: ${(props) => props.theme.borderRadii.md};
   font-size: ${(props) => props.theme.fontSizes.md};
