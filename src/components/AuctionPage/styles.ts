b9534fd2edf1f421735daf36d47ad39ebe6a4a08
hieulai <>
2021-01-23 16:44:39 +1100

    update exports

--- src/components/AuctionPage/styles.ts
+++ src/components/AuctionPage/styles.ts
@@ -2,26 +2,26 @@ import styled from 'styled-components'
 
 import Icon from '../Icons'
 
-const Title = styled.h1`
+export const Title = styled.h1`
   font-size: 3rem;
   margin-bottom: ${(props) => props.theme.space.sm};
   margin-top: 0;
 `
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
 `
 
-const Address = styled.h1`
+export const Address = styled.h1`
   color: ${(props) => props.theme.palette.grey[600]};
   font-size: ${(props) => props.theme.fontSizes.xxl};
   font-weight: ${(props) => props.theme.fontWeights.regular};
   margin: 0 ${(props) => props.theme.space.sm} 0 0;
 `
 
-const StyledIcon = styled(Icon)`
+export const StyledIcon = styled(Icon)`
   fill: ${(props) => props.theme.palette.grey[600]};
   && {
     height: 14px;
@@ -29,4 +29,4 @@ const StyledIcon = styled(Icon)`
   }
 `
 
-export { Address, Title, Wrapper, StyledIcon }
+// export { Address, Title, Wrapper, StyledIcon }
