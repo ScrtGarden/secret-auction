2be2a35a95564162eeac772fe24f1da837ee6a0f
hieulai <>
2021-01-23 16:10:34 +1100

    add validation

--- utils/validators/bid.ts
+++ utils/validators/bid.ts
@@ -1,9 +1,11 @@
-const validator = (bidAmount: string, minimumBid: string): string => {
+const validator = (bidAmount: string, minimumBid: string = '0'): string => {
   let bidAmountError = ''
   const parsedBidAmount = parseInt(bidAmount, 10)
   const parsedMinimumBid = parseInt(minimumBid, 10)
 
-  if (parsedBidAmount === 0) {
+  if (isNaN(parsedBidAmount)) {
+    bidAmountError = 'Invalid bid.'
+  } else if (parsedBidAmount === 0) {
     bidAmountError = 'Bid cannot be 0.'
   } else if (parsedBidAmount < parsedMinimumBid) {
     bidAmountError = 'Bid must be greater or equal to minimum bid.'
