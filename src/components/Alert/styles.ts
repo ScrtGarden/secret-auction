56cfe9e8ec29432cc68a68996c5753ea549215b2
hieulai <>
2021-01-23 17:03:35 +1100

    revert export

--- src/components/Alert/styles.ts
+++ src/components/Alert/styles.ts
@@ -1,10 +1,12 @@
 import { Alert } from '@zendeskgarden/react-notifications'
 import styled from 'styled-components'
 
-export const StyledAlert = styled(Alert)`
+const StyledAlert = styled(Alert)`
   position: fixed;
   right: ${(props) => props.theme.space.lg};
   top: ${(props) => props.theme.space.md};
   width: 400px;
   z-index: 401;
 `
+
+export { StyledAlert }
--- src/components/AuctionPage/styles.ts
+++ src/components/AuctionPage/styles.ts
@@ -2,29 +2,31 @@ import styled from 'styled-components'
 
 import Icon from '../Icons'
 
-export const Title = styled.h1`
+const Title = styled.h1`
   font-size: 3rem;
   margin-bottom: ${(props) => props.theme.space.sm};
   margin-top: 0;
 `
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   align-items: center;
   display: flex;
   flex-direction: row;
 `
 
-export const Address = styled.h1`
+const Address = styled.h1`
   color: ${(props) => props.theme.palette.grey[600]};
   font-size: ${(props) => props.theme.fontSizes.xxl};
   font-weight: ${(props) => props.theme.fontWeights.regular};
   margin: 0 ${(props) => props.theme.space.sm} 0 0;
 `
 
-export const StyledIcon = styled(Icon)`
+const StyledIcon = styled(Icon)`
   fill: ${(props) => props.theme.palette.grey[600]};
   && {
     height: 14px;
     width: 14px;
   }
 `
+
+export { Address, Title, Wrapper, StyledIcon }
