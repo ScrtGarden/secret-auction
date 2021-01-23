91c7abd5426b6410b311f1d0d1d3857675478e29
hieulai <>
2021-01-23 16:09:59 +1100

    return 0 if amount undefine

--- utils/toSmallestDenomination.ts
+++ utils/toSmallestDenomination.ts
@@ -1,6 +1,9 @@
 import Big from 'big.js'
 
-const toSmallestDenomination = (amount: string, decimals: number) => {
+const toSmallestDenomination = (amount: string, decimals: number = 0) => {
+  if (!amount) {
+    return '0'
+  }
   const parsedAmount = Big(amount)
   const multipleAmount = Big(10).pow(decimals)
 
