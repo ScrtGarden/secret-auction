b9575062c307f0b5e9c9438e172108cf4502f8d3
hieulai <>
2021-01-31 13:01:08 +1100

    update ui

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -1,11 +1,22 @@
+import { Player } from '@lottiefiles/react-lottie-player'
 import { FC, memo } from 'react'
 
-import { Container, StyledSpinner } from './styles'
+import LOTTIE_FILE from '../../../public/lottie/plant.json'
+import { BrandName } from '../Common/StyledComponents'
+import { Container } from './styles'
 
-const LoadingPage: FC = () => (
-  <Container>
-    <StyledSpinner size="80" />
-  </Container>
-)
+const LoadingPage: FC = () => {
+  return (
+    <Container>
+      <Player
+        autoplay
+        loop
+        src={LOTTIE_FILE}
+        style={{ width: '300px', height: '300px' }}
+      />
+      <BrandName>tulip</BrandName>
+    </Container>
+  )
+}
 
 export default memo(LoadingPage)
--- src/components/LoadingPage/styles.ts
+++ src/components/LoadingPage/styles.ts
@@ -4,12 +4,14 @@ import styled from 'styled-components'
 const Container = styled.div`
   align-items: center;
   display: flex;
+  flex-direction: column;
   flex-grow: 1;
   justify-content: center;
 `
 
 const StyledSpinner = styled(Spinner)`
-  color: ${props => props.theme.palette.green[700]};
+  color: ${(props) => props.theme.palette.green[700]};
+  margin-bottom: ${(props) => props.theme.space.lg};
 `
 
 export { Container, StyledSpinner }
