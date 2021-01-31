7ab3f43a4b8724615b6e944b09e09d63f0f86fc9
hieulai <>
2021-01-31 12:01:20 +1100

    add loading screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
+import 'react-datepicker/dist/react-datepicker.css'
+
 import { ThemeProvider } from '@zendeskgarden/react-theming'
 import { StoreProvider } from 'easy-peasy'
 import { AppProps } from 'next/app'
 
 import GlobalStyle from '../src/styles/GlobalStyle'
 import { useStore } from '../store'
+import useDocumentReady from '../utils/hooks/useDocumentReady'
 import { SecretJsContext, useCosmWasmClient } from '../utils/secretjs'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const store = useStore(pageProps.initialState)
   const client = useCosmWasmClient()
 
+  // custom hook
+  const { ready } = useDocumentReady()
+
   return (
     <StoreProvider store={store}>
       <SecretJsContext.Provider value={{ secretjs: client }}>
         <ThemeProvider focusVisibleRef={null}>
           <GlobalStyle />
-          <Component {...pageProps} />
+          {ready ? <Component {...pageProps} /> : <div>Loading...</div>}
         </ThemeProvider>
       </SecretJsContext.Provider>
     </StoreProvider>
