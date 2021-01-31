40bff91b1fbd9a7ddff41c2a7cebbc4c3b9b7073
hieulai <>
2021-01-31 12:03:23 +1100

    add window on load hook

---
+++ utils/hooks/useDocumentReady.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+const useDocumentReady = () => {
+  const [ready, setReady] = useState(false)
+
+  useEffect(() => {
+    let isMounted = true
+    if (document.readyState === 'complete') {
+      if (isMounted) {
+        setReady(true)
+      }
+    } else {
+      window.onload = () => {
+        if (isMounted) {
+          setReady(true)
+        }
+      }
+    }
+
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  return {
+    ready,
+  }
+}
+
+export default useDocumentReady
