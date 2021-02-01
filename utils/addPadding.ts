c86116c190e24e1d704c72d923a2584f14a496e2
hieulai <>
2021-02-01 22:37:42 +1100

    change to $

--- utils/addPadding.ts
+++ utils/addPadding.ts
@@ -1,6 +1,6 @@
 const addPadding = (amount: string) => {
   const paddingLength = 40 - amount.length
-  return ' '.repeat(paddingLength)
+  return '$'.repeat(paddingLength)
 }
 
 export default addPadding
