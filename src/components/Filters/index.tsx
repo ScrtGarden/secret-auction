d7a750bef7dd3f85f03856bd4423e32e00be286f
hieulai <>
2021-02-03 22:17:56 +1100

    integrate filter

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -6,19 +6,35 @@ import InputWithDropdown from '../Common/InputWithDropdown'
 import { Container, StartIcon } from './styles'
 
 type Props = {
+  searchValue: string
+  onChangeSearchValue: (value: string) => void
   options: FilterToken[]
   sellValue: string
   onChangeSellValue: (value: string) => void
+  bidValue: string
+  onChangeBidValue: (value: string) => void
 }
 
 const Filters: FC<Props> = (props) => {
-  const { options, sellValue, onChangeSellValue } = props
+  const {
+    searchValue,
+    onChangeSearchValue,
+    options,
+    sellValue,
+    onChangeSellValue,
+    bidValue,
+    onChangeBidValue,
+  } = props
 
   return (
     <Container>
       <Field>
         <Label>Search</Label>
-        <MediaInput start={<StartIcon name="search" />} />
+        <MediaInput
+          start={<StartIcon name="search" />}
+          value={searchValue}
+          onChange={(e) => onChangeSearchValue(e.currentTarget.value)}
+        />
       </Field>
       <InputWithDropdown
         label="Sell"
@@ -26,7 +42,12 @@ const Filters: FC<Props> = (props) => {
         value={sellValue}
         onChange={onChangeSellValue}
       />
-      <InputWithDropdown label="Bid" options={options} />
+      <InputWithDropdown
+        label="Bid"
+        options={options}
+        value={bidValue}
+        onChange={onChangeBidValue}
+      />
     </Container>
   )
 }
