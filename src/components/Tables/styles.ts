974a9de4f055ea01f676c98dcffe681bd2c062b6
hieulai <>
2021-02-06 11:15:07 +1100

    pair symbol styles

---
+++ src/components/Tables/styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+interface SymbolProps {
+  readonly bid?: boolean
+}
+
+const Pair = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const Symbol = styled.p<SymbolProps>`
+  color: ${(props) => props.theme.palette.grey[800]};
+  font-size: ${(props) => props.theme.fontSizes.md};
+  font-weight: ${(props) => props.theme.fontWeights.semibold};
+  margin: 0;
+
+  ${(props) =>
+    props.bid &&
+    `
+    color: ${props.theme.palette.grey[500]};
+    font-size: ${props.theme.fontSizes.md};
+    font-weight: ${props.theme.fontWeights.regular};
+  `}
+`
+
+export { Pair, Symbol }
