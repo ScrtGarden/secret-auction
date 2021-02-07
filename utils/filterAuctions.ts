63cea60594d78b6f53fb87532918a639d554aafc
hieulai <>
2021-02-07 22:42:19 +1100

    match symbol

--- utils/filterAuctions.ts
+++ utils/filterAuctions.ts
@@ -14,18 +14,19 @@ const filter = (auctions: CombinedAuctionInfo[], filters: Filters) => {
     return auctions
   }
 
+  const sellPattern = `^${sellSymbol.toUpperCase()}`
+  const sellRegex = new RegExp(sellPattern)
+  const bidPattern = `^${bidSymbol.toUpperCase()}`
+  const bidRegex = new RegExp(bidPattern)
+
   return auctions.filter((item) => {
     const { bidTokenSymbol, sellTokenSymbol } = splitPair(item.pair)
 
     const selectedBidSymbolMatch = selectedBidSymbol
       ? selectedBidSymbol === bidTokenSymbol
       : true
-    const sellSymbolMatch = sellSymbol
-      ? sellTokenSymbol.includes(sellSymbol.toUpperCase())
-      : true
-    const bidSymbolMatch = bidSymbol
-      ? bidTokenSymbol.includes(bidSymbol.toUpperCase())
-      : true
+    const sellSymbolMatch = sellSymbol ? sellTokenSymbol.match(sellRegex) : true
+    const bidSymbolMatch = bidSymbol ? bidTokenSymbol.match(bidRegex) : true
 
     return selectedBidSymbolMatch && sellSymbolMatch && bidSymbolMatch
   })
