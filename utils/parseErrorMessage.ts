bf5231bef981b08f9d4bcefe70a8afa82f369067
hieulai <>
2021-02-09 22:09:31 +1100

    update wording

--- utils/parseErrorMessage.ts
+++ utils/parseErrorMessage.ts
@@ -2,9 +2,9 @@ const parseErrorMessage = (message: string) => {
   let parsedMessage = 'Something went wrong. Please try again later.'
 
   if (message.includes('Request rejected')) {
-    parsedMessage = 'Looks like you rejected the request.'
+    parsedMessage = 'Request rejected.'
   } else if (message.includes('out of gas')) {
-    parsedMessage = 'Sorry, you ran out of gas.'
+    parsedMessage = 'Sorry you ran out of gas.'
   }
 
   return parsedMessage
