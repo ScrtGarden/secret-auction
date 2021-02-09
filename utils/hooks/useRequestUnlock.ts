c159e225346e7d0952cd8bfcf74bb0230178db2a
hieulai <>
2021-02-09 22:10:34 +1100

    use connect to keplr hook

--- utils/hooks/useRequestUnlock.ts
+++ utils/hooks/useRequestUnlock.ts
@@ -1,24 +1,31 @@
 import { useState } from 'react'
 
 import keplr from '../keplr'
+import useConnectToKeplr from './useConnectToKeplr'
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 const useRequestUnlock = (tokenAddress: string = '') => {
+  // custom hook
+  const [connectToKelpr] = useConnectToKeplr()
+
+  // hook state
   const [loading, setLoading] = useState(false)
   const [success, setSuccess] = useState(false)
 
   const requestUnlock = async () => {
     setLoading(true)
-    const keplrClient = keplr.getKeplr()
 
-    if (!keplrClient) {
-      console.log('Keplr not installed.')
+    const { error } = await connectToKelpr()
+
+    if (error) {
       setLoading(false)
       return
     }
+
+    const keplrClient = keplr.getKeplr()
 
     try {
       await keplrClient.suggestToken(
