4803be9569272230b5b734ed3c2ff2e164aeefd0
hieulai <>
2021-02-13 16:57:32 +1100

    add icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -3,7 +3,9 @@ import ChevronDown from './ChevronDown'
 import Clock from './Clock'
 import Copy from './Copy'
 import Dizzy from './Dizzy'
+import DreamWorld from './DreamWorld'
 import Farming from './Farming'
+import Heatmap from './Heatmap'
 import InfoCircle from './InfoCircle'
 import Keplr from './Kelpr'
 import KeySkeleton from './KeySkeleton'
@@ -13,6 +15,7 @@ import Seedling from './Seedling'
 import Shovel from './Shovel'
 import StatsMan from './StatsMan'
 import Telescope from './Telescope'
+import TransferMoney from './TransferMoney'
 import Tulip from './Tulip'
 import User from './User'
 import UserCircle from './UserCircle'
@@ -29,6 +32,15 @@ const Icon = (props: Props) => {
   let SelectedIcon
 
   switch (name) {
+    case 'heatmap':
+      SelectedIcon = Heatmap
+      break
+    case 'transfer-money':
+      SelectedIcon = TransferMoney
+      break
+    case 'dream-world':
+      SelectedIcon = DreamWorld
+      break
     case 'stats-man':
       SelectedIcon = StatsMan
       break
