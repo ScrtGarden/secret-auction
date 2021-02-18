3796076bc5830be7fbcc37fffba8722d67ce543b
hieulai <>
2021-02-18 20:53:32 +1100

    update padding

--- src/components/HomePage/Validator/styles.ts
+++ src/components/HomePage/Validator/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.palette.grey[300]};
-  height: 30rem;
+  padding: 70px 0;
 `
 
 export { Container }
