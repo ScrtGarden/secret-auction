41cdb92dda14eba6bc625f8f072019a1c354934a
hieulai <>
2021-02-27 14:13:02 +1100

    refactor

--- src/components/Modals/Bid/index.tsx
+++ src/components/Modals/Bid/index.tsx
@@ -3,23 +3,17 @@ import { useRouter } from 'next/router'
 import { FormEvent, useState } from 'react'
 
 import { BidRouterQuery } from '../../../../interfaces'
-import { AlertType } from '../../../../store/controls/controls.models'
-import addPadding from '../../../../utils/addPadding'
-import { PLACE_BID_MAX_GAS } from '../../../../utils/constants'
 import { useStoreActions } from '../../../../utils/hooks/storeHooks'
-import useConnectToKeplr from '../../../../utils/hooks/useConnectToKeplr'
 import useGetAuction from '../../../../utils/hooks/useGetAuction'
-import keplr from '../../../../utils/keplr'
-import parseErrorMessage from '../../../../utils/parseErrorMessage'
-import toSmallestDenomination from '../../../../utils/toSmallestDenomination'
-import validator from '../../../../utils/validators/bid'
+import usePlaceBid from '../../../../utils/hooks/usePlaceBid'
 import {
   ModalHeader,
   ModalTitle,
   StyledModal,
 } from '../../Common/StyledComponents'
+import SuccessBid from '../../SuccessBid'
 import Details from './Details'
-import Success from './Success'
+import { SuccessBidWrapper } from './styles'
 
 const BidModal = () => {
   const router = useRouter()
@@ -27,20 +21,15 @@ const BidModal = () => {
 
   // custom hooks
   const { loading: loadingAuctionInfo, data, error } = useGetAuction(address)
-  const [connectToKeplr] = useConnectToKeplr()
+  const { error: placeBidError, loading, placeBid, txHash } = usePlaceBid()
 
   // store actions
   const toggleModal = useStoreActions(
     (actions) => actions.controls.toggleBidModal
   )
-  const setAlert = useStoreActions((actions) => actions.controls.setAlertInfo)
 
   // component state
-  const [amount, setAmount] = useState('0')
-  const [bidAmountError, setBidAmountError] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [success, setSuccess] = useState(false)
-  const [txHash, setTxHash] = useState<string | undefined>('')
+  const [amount, setAmount] = useState('')
 
   const onClose = () => {
     toggleModal()
@@ -61,62 +50,13 @@ const BidModal = () => {
   }
 
   const onSubmit = async () => {
-    setLoading(true)
-
-    const { error: connectionError } = await connectToKeplr()
-
-    if (connectionError) {
-      setLoading(false)
-      return
-    }
-
-    setBidAmountError('')
-    const amountInSmallestDenomination = toSmallestDenomination(
+    await placeBid({
       amount,
-      data?.bid_token.token_info.decimals || 0
-    )
-    const amountError = validator(
-      amountInSmallestDenomination,
-      data?.minimum_bid
-    )
-
-    if (amountError) {
-      setBidAmountError(amountError)
-      setLoading(false)
-      return
-    }
-
-    // place bid
-    const handleMsg = {
-      send: {
-        recipient: data?.auction_address,
-        amount: amountInSmallestDenomination,
-        padding: addPadding(amountInSmallestDenomination),
-      },
-    }
-
-    const { secretjs: signingClientOne } = await keplr.createSigningClient({
-      maxGas: PLACE_BID_MAX_GAS,
+      minimumBid: data?.minimum_bid || '',
+      auctionAddress: address,
+      tokenAddress: data?.bid_token.contract_address || '',
+      decimals: data?.bid_token.token_info.decimals || 0,
     })
-
-    try {
-      const response = await signingClientOne?.execute(
-        data?.bid_token.contract_address || '',
-        handleMsg
-      )
-      console.log(response)
-      setTxHash(response?.transactionHash)
-      setSuccess(true)
-    } catch (error) {
-      console.log('Error placing bid:', error.message)
-      const text = parseErrorMessage(error.message)
-      setAlert({
-        title: 'Error',
-        text,
-        type: AlertType.error,
-      })
-      setLoading(false)
-    }
   }
 
   return (
@@ -125,7 +65,7 @@ const BidModal = () => {
         <ModalTitle>Bid</ModalTitle>
         <Close />
       </ModalHeader>
-      {!success ? (
+      {!txHash ? (
         <Details
           endsAt={data?.ends_at}
           sellAmount={data?.sell_amount}
@@ -137,19 +77,21 @@ const BidModal = () => {
           label="Amount"
           value={amount}
           onChange={onChangeAmount}
-          error={bidAmountError}
+          error={placeBidError}
           bidding={loading}
           bidError={!!error}
           onSubmit={onSubmit}
           loading={loading}
         />
       ) : (
-        <Success
-          amount={amount}
-          symbol={data?.bid_token.token_info.symbol}
-          onClick={onClose}
-          txHash={txHash}
-        />
+        <SuccessBidWrapper>
+          <SuccessBid
+            amount={amount}
+            symbol={data?.bid_token.token_info.symbol}
+            onClick={onClose}
+            txHash={txHash}
+          />
+        </SuccessBidWrapper>
       )}
     </StyledModal>
   )
---
+++ src/components/Modals/Bid/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const SuccessBidWrapper = styled.div`
+  padding: ${(props) => props.theme.space.md} ${(props) => props.theme.space.lg};
+`
+
+export { SuccessBidWrapper }
