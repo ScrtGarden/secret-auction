3a33cd65a0e2b8d67ee2c881fda1adf6420feafb
hieulai <>
2021-03-21 16:40:27 +1100

    🏷️ add page type

---
+++ types/page.d.ts
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+import { ComponentType, ReactElement, ReactNode } from 'react'
+
+export type Page<P = {}> = NextPage<P> & {
+  // You can disable whichever you don't need
+  getLayout?: (page: ReactElement) => ReactNode
+}
